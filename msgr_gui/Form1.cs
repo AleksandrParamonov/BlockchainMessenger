067480826918627dad25cf7ec9b5bd6fa900d30f
JBloodless <jack.bloodless@gmail.com>
2017-09-22 16:52:25 +0300

    Add files via upload
    GUI embryo

---
+++ msgr_gui/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace msgr_gui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            richTextBox1.Enabled = false;
+            richTextBox2.Enabled = false;
+            button1.Enabled = false;
+        }
+
+        private void серверToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
